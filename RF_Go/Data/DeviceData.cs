069177f694881e09fc8f1799e3aee154a3e449bc
christophe bouserez <c_bouserez@hotmail.com>
2023-12-28 17:27:46 +0100

    Need to change the way components interacts with each other

---
+++ RF_Go/Data/DeviceData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RF_Go.Data
+{
+    public class DeviceData
+    {
+        public Dictionary<string, Dictionary<string, Dictionary<string, List<int>>>> Brands { get; set; }
+    }
+}
