60542439532c435e02324debbda0b6148d1a21cb
Beatwen <c_bouserez@hotmail.com>
2024-10-06 21:03:07 +0200

    Colors and styles added. Mostly frontend stuff.

--- RF_Go/Models/RFDevice.cs
+++ RF_Go/Models/RFDevice.cs
@@ -20,6 +20,7 @@ namespace RF_Go.Models
         public bool Selected { get; set; }
         public string Brand { get; set; }
         public string Model { get; set; }
+        public string Name { get; set; }
         public string Frequency { get; set; }
         [Ignore]
         public List<int> Range { get; set; }
