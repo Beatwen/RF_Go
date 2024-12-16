858493b3b2b74378fdc778baa4bc27c6922f09db
Beatwen <c_bouserez@hotmail.com>
2024-12-16 21:21:18 +0100

    Licensing - V1

--- RF_Go/Models/Licensing/User.cs
+++ RF_Go/Models/Licensing/User.cs
@@ -8,12 +8,12 @@ namespace RF_Go.Models.Licensing
 {
     public class User
     {
-        public string userName;
-        public string firstName;
-        public string lastName;
-        public int id;
-        public string email;
-        public string createdAt;
-        public string updatedAt;
+        public string userName { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int id { get; set; }
+        public string email { get; set; }
+        public string createdAt { get; set; }
+        public string updatedAt { get; set; }
     }
 }
