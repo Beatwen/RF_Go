5ba93b3c82f6eee55e035742ee1b85972cb7a9fe
Beatwen <c_bouserez@hotmail.com>
2025-04-13 18:12:08 +0200

    Deploy website - based on 35c72ab60056e9fbdf2935484204ccb4c2b57b4f

--- RF_Go/Licensing/Models/License.cs
+++
@@ -1,9 +0,0 @@
-namespace MyApp.Licensing.Models;
-
-public class License
-{
-    public string DeviceId { get; set; }
-    public string LicenseKey { get; set; }
-    public DateTime ExpirationDate { get; set; }
-    public bool IsValid { get; set; }
-}
---
+++ assets/js/9669.20ddcd75.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkrf_go_docs=self.webpackChunkrf_go_docs||[]).push([[9669],{9669:(e,s,c)=>{c.d(s,{createGitGraphServices:()=>r.b});var r=c(7123);c(3579)}}]);
