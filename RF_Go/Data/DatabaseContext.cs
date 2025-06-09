f85bbc11d99476f006d84b3304a2be2155b1b04b
Beatwen <c_bouserez@hotmail.com>
2025-06-09 17:52:57 +0200

    db name is changed and tweaked devices frequencies in the dataset

--- RF_Go/Data/DatabaseContext.cs
+++ RF_Go/Data/DatabaseContext.cs
@@ -12,7 +12,7 @@ namespace RF_Go.Data
 {
     public class DatabaseContext : IAsyncDisposable
     {
-        private const string DbName = "RF_Go8.db3";
+        private const string DbName = "RFGo.db3";
         public static string DbPath => Path.Combine(FileSystem.AppDataDirectory, DbName);
 
         private SQLiteAsyncConnection _connection;
